45e3c648b08b09d515779ada73758111d4b845d8
Marcelo <marmon@hotmail.com.br>
2020-10-13 15:22:22 -0300

    Criado atributo de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,18 @@
 class Pessoa:
+    #
+    def __init__(self, nome=None, idade=35):
+        """
+        metodo construtor (primeiro a rodar)
+
+        :param nome:
+        """
+        # atributos
+        self.nome = nome
+        self.idade = idade
+        
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(p.cumprimentar())
-
