5ab0ac22bdb67264c31461252b61ccc5dc0da899
Marcelo <marmon@hotmail.com.br>
2020-10-13 20:18:44 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,25 @@
 class Pessoa:
     #
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         """
         metodo construtor (primeiro a rodar)
 
+        :param *filhos:
         :param nome:
         """
         # atributos
         self.nome = nome
         self.idade = idade
-        
-    def cumprimentar(self):
+        self.filhos = filhos
+
+    def cumprimentar(self) -> object:
         return 'Olá'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
+    marcelo = Pessoa(nome='marcelo')
+    luciano = Pessoa(marcelo, nome='luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
